cfdfef2c18fdc7a40d7a690d5cb07aec6e59d921
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2024-03-02 11:50:46 +0100

    Did task 1.1 and 1.2

---
+++ part1/src/App.jsx
@@ -0,0 +1,38 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Part = ({part, exercise}) => (
+    <p>
+      {part} {exercise}
+    </p>
+  )
+
+  const Header = ({course}) => (<h1>{course}</h1>);
+  const Content = () => (
+    <>
+      <Part part = {part1} exercise = {exercises1} />
+      <Part part = {part2} exercise = {exercises2} />
+      <Part part = {part3} exercise = {exercises3} />
+    </>
+  );
+
+  const Total = () => (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  );
+
+  return (
+    <div>
+      <Header course = {course} />
+      <Content />
+      <Total />
+    </div>
+  );
+};
+
+export default App
