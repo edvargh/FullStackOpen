89f8fa59dc0b2138bc7234c8776b3953ce3dc133
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2024-03-03 20:28:53 +0100

    Did task 2.5

--- part2/courseinfo2/src/App.jsx
+++ part2/courseinfo2/src/App.jsx
@@ -1,28 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Total of {sum} exercises</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-<>
-  {parts.map(part => <Part key = {part.id} part = {part}/>)}
-</>
-
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={totalExercises} />
-    </div>
-  );
-};
+import Course from './Course';
 
 const App = () => {
   const courses = [
