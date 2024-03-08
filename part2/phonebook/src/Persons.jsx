7649f17c129c7ea83fc7bdeccc035059eca90031
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2024-03-08 16:00:40 +0100

    Fixed an error

--- part2/phonebook/src/Persons.jsx
+++ part2/phonebook/src/Persons.jsx
@@ -1,15 +1,17 @@
-const Person = ({ name, number }) => {
-    return (
-      <div>{name} {number}</div>
-    );
-  };
-
-  const Persons = ({ persons }) => {
+const Persons = ({ persons }) => {
     return (
       <div>
         {persons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
       </div>
     );
   };
+
+  const Person = ({ name, number }) => {
+    return (
+      <div>{name} {number}</div>
+    );
+  };
+
+  
 
 export default Persons;
