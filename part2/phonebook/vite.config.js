5b8026790c5ce2345e077e22556b2c9a5a360e05
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2024-05-05 10:59:52 +0200

    Further adjustments

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Ensure this is pointing to 'dist' and not 'dist/assets'
+    assetsDir: 'assets',  // This will put other assets in /dist/assets
+    rollupOptions: {
+      // Additional Rollup options if needed
+    }
+  }
+});
